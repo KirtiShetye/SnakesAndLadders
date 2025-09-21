82afcb0c91dbd07edc324fbbac66d3bd6757c54c
KirtiShetye <kirtishetye1234@gmail.com>
2025-09-21 23:19:29 +0530

    Added logic in GameService class

--- src/main/java/com/example/snakesandladders/service/GameService.java
+++ src/main/java/com/example/snakesandladders/service/GameService.java
@@ -1,4 +1,25 @@
 package com.example.snakesandladders.service;
 
+import com.example.snakesandladders.model.Board;
+
 public class GameService {
+
+    private final Board board;
+    public GameService(){
+        this.board = Board.getInstance();
+    }
+
+    public int checkForSnakeOrLadder(int position, int diceCount) {
+        int newPosition=position+diceCount;
+        if(newPosition > Board.BOARD_MAX) {
+            System.out.println("New position range exceeds maximum");
+            return position;
+        }
+        else if(newPosition < Board.BOARD_MAX) {
+            return board.applyJump(newPosition);
+        }
+        else{
+            return Board.BOARD_MAX;
+        }
+    }
 }
