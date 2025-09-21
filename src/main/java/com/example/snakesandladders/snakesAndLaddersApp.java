a192e9721ed9f466c9e3fbdaae52d05725886e02
KirtiShetye <kirtishetye1234@gmail.com>
2025-09-21 23:26:51 +0530

    Added logic in Application Start

--- src/main/java/com/example/snakesandladders/snakesAndLaddersApp.java
+++ src/main/java/com/example/snakesandladders/snakesAndLaddersApp.java
@@ -1,4 +1,10 @@
 package com.example.snakesandladders;
 
+import com.example.snakesandladders.controller.GameController;
+
 public class snakesAndLaddersApp {
+    public static void main(String[] args) {
+        GameController gameController = new GameController();
+        gameController.startGame();
+    }
 }
